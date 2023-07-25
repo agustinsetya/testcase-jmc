0b5f86ef0ce658f46b388efc57820a859f22edca
Agustin Setyo Rini <agustin.setyo@majoo.id>
2023-07-25 23:21:06 +0700

    Delete regency.js
    Remove unused code

--- public/assets/javascripts/regency.js
+++
@@ -1,9 +0,0 @@
-(function ($) {
-    'use strict';
-
-    if ($('.btn-detail-product').click()) {
-        var id = $('.btn-detail-product').val()
-        $('.product-detail-modal').attr('id', '"modal_detail_product' + id + '"')
-    }
-
-}).apply(this, [jQuery]);
